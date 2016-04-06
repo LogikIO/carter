2c2cda12ed7f9515044b342ab274a47f927ee9e3
Nicky Woolf <woolfnicky@gmail.com>
2016-04-06 12:34:20 -0700

    Include default registration form.

--- src/views/shopify/auth/login.blade.php
+++ src/views/shopify/auth/login.blade.php
@@ -4,4 +4,4 @@
     <script type="text/javascript">
         window.top.location.href = '{{ $redirect }}';
     </script>
-@endsection
+@stop
---
+++ src/views/shopify/stand_alone.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    @yield('head')
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+
+@yield('content')
+
+</body>
+</html>
