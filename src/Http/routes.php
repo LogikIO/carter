29b94647fe6cf3bf630a8b84bd2047b72404bbc8
Nicky Woolf <woolfnicky@gmail.com>
2016-04-13 10:30:14 -0700

    Update routes array key for uri.

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -3,33 +3,33 @@
 Route::group(['middleware' => 'web'], function ($router) {
 
     $router->get(
-        config('carter.shopify.routes.signup.url'),
+        config('carter.shopify.routes.signup.uri'),
         config('carter.shopify.routes.signup.action')
     )->name('shopify.signup');
 
     $router->match(
         ['get', 'post'],
-        config('carter.shopify.routes.install.url'),
+        config('carter.shopify.routes.install.uri'),
         config('carter.shopify.routes.install.action')
     )->name('shopify.install');
 
     $router->get(
-        config('carter.shopify.routes.register.url'),
+        config('carter.shopify.routes.register.uri'),
         config('carter.shopify.routes.register.action')
     )->name('shopify.register');
 
     $router->get(
-        config('carter.shopify.routes.activate.url'),
+        config('carter.shopify.routes.activate.uri'),
         config('carter.shopify.routes.activate.action')
     )->name('shopify.activate');
 
     $router->get(
-        config('carter.shopify.routes.login.url'),
+        config('carter.shopify.routes.login.uri'),
         config('carter.shopify.routes.login.action')
     )->name('shopify.login');
 
     $router->get(
-        config('carter.shopify.routes.dashboard.url'),
+        config('carter.shopify.routes.dashboard.uri'),
         config('carter.shopify.routes.dashboard.action')
     )->name('shopify.dashboard');
 
