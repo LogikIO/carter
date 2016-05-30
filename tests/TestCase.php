2bd6bff049ae797dc5ec372c892957abbdab6fe9
Nicky Woolf <woolfnicky@gmail.com>
2016-05-30 16:56:46 -0700

    Autoload TestCase.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,17 +1,11 @@
 <?php
 
-namespace Woolf\Carter\Tests;
-
-use Mockery as m;
-use PHPUnit_Framework_TestCase;
-
 abstract class TestCase extends PHPUnit_Framework_TestCase
 {
-
     public function tearDown()
     {
-        if (class_exists('Mockery')) {
-            m::close();
+        if (class_exists(Mockery::class)) {
+            Mockery::close();
         }
     }
 }
