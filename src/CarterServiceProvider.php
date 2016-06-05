2dba599736627dbc6e2d16ffbd8dbb7febb04cec
Nicky Woolf <woolfnicky@gmail.com>
2016-06-05 09:42:55 -0700

    Require routes after merging config. Clean up service provider.

--- src/CarterServiceProvider.php
+++ src/CarterServiceProvider.php
@@ -14,23 +14,27 @@ class CarterServiceProvider extends ServiceProvider
 
     public function boot()
     {
+        $this->loadViewsFrom($this->packageViews(), 'carter');
+        $this->publishes([$this->packageViews() => base_path('resources/views/vendor/carter')]);
+
+        $this->publishes([$this->packageConfig() => config_path('carter.php')], 'config');
+        $this->mergeConfigFrom($this->packageConfig(), 'carter');
+
+        $this->commands('command.carter.table');
+
         if (! $this->app->routesAreCached()) {
             require __DIR__.'/Http/routes.php';
         }
+    }
 
-        $this->loadViewsFrom(__DIR__.'/views', 'carter');
+    protected function packageViews()
+    {
+        return __DIR__.'/views';
+    }
 
-        $this->publishes([
-            __DIR__.'/views' => base_path('resources/views/vendor/carter'),
-        ]);
-
-        $this->publishes([
-            __DIR__.'/config/carter.php' => config_path('carter.php')
-        ], 'config');
-
-        $this->mergeConfigFrom(__DIR__.'/config/carter.php', 'carter');
-
-        $this->commands('command.carter.table');
+    protected function packageConfig()
+    {
+        return __DIR__.'/config/carter.php';
     }
 
     public function register()
@@ -39,26 +43,35 @@ class CarterServiceProvider extends ServiceProvider
             return $app->make($app->make('config')->get('auth.providers.users.model'));
         });
 
-        $this->app->when(Endpoint::class)
-            ->needs('$domain')
-            ->give(function () {
-                return ($user = auth()->user()) ? $user->domain : request('shop');
-            });
+        $this->app->when(Endpoint::class)->needs('$domain')->give($this->myshopifyDomain());
 
-        $this->app->when(Client::class)
-            ->needs('$accessToken')
-            ->give(function () {
-                return (auth()->check()) ? auth()->user()->access_token : null;
-            });
+        $this->app->when(Client::class)->needs('$accessToken')->give($this->accessToken());
 
-        $this->app->when(Signature::class)
-            ->needs('$request')
-            ->give(function () {
-                return request()->all();
-            });
+        $this->app->when(Signature::class)->needs('$request')->give($this->requestArray());
 
         $this->app->singleton('command.carter.table', function () {
             return new CarterTableCommand();
         });
+    }
+
+    protected function myshopifyDomain()
+    {
+        return function () {
+            return ($user = auth()->user()) ? $user->domain : request('shop');
+        };
+    }
+
+    protected function accessToken()
+    {
+        return function () {
+            return (auth()->check()) ? auth()->user()->access_token : null;
+        };
+    }
+
+    protected function requestArray()
+    {
+        return function () {
+            return request()->all();
+        };
     }
 }
