7ffb70f074de3301e52b508bca62fa0992feac95
Nicky Woolf <woolfnicky@gmail.com>
2016-10-09 15:57:51 -0700

    Group middleware in routes file.

--- src/Carter/Laravel/routes.php
+++ src/Carter/Laravel/routes.php
@@ -1,23 +1,25 @@
 <?php
 
-Route::get('shopify/signup', 'NickyWoolf\Carter\Laravel\ShopifyController@signupForm')
-    ->name('shopify.signup');
+Route::group(['middleware' => 'web'], function () {
+    Route::get('shopify/signup', 'NickyWoolf\Carter\Laravel\ShopifyController@signupForm')
+        ->name('shopify.signup');
 
-Route::match(['get', 'post'], 'shopify/install', 'NickyWoolf\Carter\Laravel\ShopifyController@install')
-    ->middleware(['carter.guest', 'carter.domain'])
-    ->name('shopify.install');
+    Route::match(['get', 'post'], 'shopify/install', 'NickyWoolf\Carter\Laravel\ShopifyController@install')
+        ->middleware(['carter.guest', 'carter.domain'])
+        ->name('shopify.install');
 
-Route::get('shopify/register', 'NickyWoolf\Carter\Laravel\ShopifyController@register')
-    ->middleware(['carter.guest', 'carter.domain', 'carter.signed', 'carter.nonce', 'carter.signature'])
-    ->name('shopify.register');
+    Route::get('shopify/register', 'NickyWoolf\Carter\Laravel\ShopifyController@register')
+        ->middleware(['carter.guest', 'carter.domain', 'carter.signed', 'carter.nonce', 'carter.signature'])
+        ->name('shopify.register');
 
-Route::get('shopify/activate', 'NickyWoolf\Carter\Laravel\ShopifyController@activate')
-    ->middleware(['carter.charged'])
-    ->name('shopify.activate');
+    Route::get('shopify/activate', 'NickyWoolf\Carter\Laravel\ShopifyController@activate')
+        ->middleware(['carter.charged'])
+        ->name('shopify.activate');
 
-Route::get('shopify/login', 'NickyWoolf\Carter\Laravel\ShopifyController@login')
-    ->middleware(['carter.guest', 'carter.domain', 'carter.signature'])
-    ->name('shopify.login');
+    Route::get('shopify/login', 'NickyWoolf\Carter\Laravel\ShopifyController@login')
+        ->middleware(['carter.guest', 'carter.domain', 'carter.signature'])
+        ->name('shopify.login');
+});
 
 Route::get('shopify/dashboard', 'NickyWoolf\Carter\Laravel\ShopifyController@dashboard')
     ->middleware(['carter.auth', 'carter.paying'])
